8435f13b383626b87517744b90e372c55c15cf90
mattia fiorio <mattia.fiorio@akqa.com>
2024-09-17 10:33:23 +0200

    Update data push script for resilience

--- src/scripts/algolia-push-data.ts
+++ src/scripts/algolia-push-data.ts
@@ -5,12 +5,19 @@ import data from "./algolia-data";
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
+if (!process.env.ALGOLIA_APP_ID)
+  throw new Error("ALGOLIA_APP_ID is not defined");
+if (!process.env.ALGOLIA_API_KEY)
+  throw new Error("ALGOLIA_API_KEY is not defined");
+if (!process.env.ALGOLIA_INDEX_NAME)
+  throw new Error("ALGOLIA_INDEX_NAME is not defined");
+
 const client = algoliasearch(
-  process.env.ALGOLIA_APP_ID || "",
-  process.env.ALGOLIA_API_KEY || "",
+  process.env.ALGOLIA_APP_ID,
+  process.env.ALGOLIA_API_KEY,
 );
 
 client.saveObjects({
-  indexName: process.env.INDEX_NAME || "test",
+  indexName: process.env.ALGOLIA_INDEX_NAME,
   objects: data,
 });
